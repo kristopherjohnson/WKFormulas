17fa8b893c4c0acc04d949849bdd52542b91a73b
Kristopher Johnson <kris@kristopherjohnson.net>
2026-03-28 16:52:45 -0400

    Initial commit
    Print all tonality formulas from "An Improviser's OS" by Wayne Krantz.
    
    Co-Authored-By: Kris Johnson <kris@kristopherjohnson.net>

---
+++ README.md
@@ -0,0 +1,17 @@
+# WKFormulas
+
+Prints all tonality formulas (1-note through 12-note) in Markdown format, as described in *An Improviser's OS* by Wayne Krantz.
+
+Each formula starts with "1" (the root) and lists interval names using standard notation (♭2, 2, ♭3, 3, 4, ♭5, 5, ♭6, 6, ♭7, 7).
+
+## Build
+
+    go build
+
+## Run
+
+    ./WKFormulas
+
+## License
+
+This project is dedicated to the public domain under [CC0 1.0](LICENSE).
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+// Prints tonality formulas, as in "An Improviser's OS" by Wayne Krantz, in Markdown format
+
+import (
+	"fmt"
+	"math/bits"
+)
+
+func main() {
+	for i := 1; i <= 12; i++ {
+		printFormulasOfLength(i)
+		if i != 12 {
+			fmt.Println()
+		}
+	}
+}
+
+var intervalNames = []string{
+	"♭2", "2", "♭3", "3", "4", "♭5", "5", "♭6", "6", "♭7", "7",
+}
+
+func printFormulasOfLength(length int) {
+	fmt.Printf("## %d-Note Formulas\n", length)
+
+	if length == 1 {
+		fmt.Println("- 1")
+		return
+	}
+
+	numFormulas := uint(1) << len(intervalNames)
+	for bitmask := uint(1); bitmask < numFormulas; bitmask++ {
+		if bits.OnesCount(bitmask) == (length - 1) {
+			printFormula(bitmask)
+		}
+	}
+}
+
+func printFormula(bitmask uint) {
+	fmt.Print("- 1")
+	for i := 0; i < len(intervalNames); i++ {
+		if bitmask&(1<<i) != 0 {
+			fmt.Printf(" %s", intervalNames[i])
+		}
+	}
+	fmt.Println()
+}
